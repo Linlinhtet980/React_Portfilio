a894328684e73f19dd44e968c865d431d7281125
Linlinhtet980 <linpop890@gmail.com>
2026-09-19 14:39:31 +0630

    Remove 'Designed & built with intention.' from Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,9 @@
-﻿import styles from './Footer.module.css'
+import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
     <footer className={['container', styles.footer].join(' ')}>
       <span>© 2026 Lin Thu Rein Htet</span>
-      <span>Designed & built with intention.</span>
       <a href="#top">Back to top ↑</a>
     </footer>
   )
